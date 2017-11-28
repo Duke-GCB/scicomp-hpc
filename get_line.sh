57f4654c64e4facd36e0e05830d29739725d4db6
John Bradley <johnbradley2008@gmail.com>
2017-11-28 15:25:17 -0500

    zero based get_line.sh

--- get_line.sh
+++ get_line.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Print out a single line from data piped into this script
+# linenum is a zero based index
 # Usage: ls <pattern> | ./get_line.sh <linenum>
-LINENUM=$1
+LINENUM=$(($1 + 1))  # add 1 to argument since sed is 1 based
 sed -n ${LINENUM}p
