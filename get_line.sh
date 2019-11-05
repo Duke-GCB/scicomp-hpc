9b24b0e22d43394ad7827d80d2a185456509e941
Dan Leehr <dan.leehr@duke.edu>
2019-11-05 10:04:26 -0500

    Remove get_line.sh
    - using `ls data/*.fasta | head -n $L | tail -n 1` instead

--- get_line.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Print out a single line from data piped into this script
-# linenum is a zero based index
-# Usage: ls <pattern> | ./get_line.sh <linenum>
-LINENUM=$(($1 + 1))  # add 1 to argument since sed is 1 based
-sed -n ${LINENUM}p
